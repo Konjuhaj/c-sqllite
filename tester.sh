35a997fe89f25adc8cc44c273a447c4198223dec
Besnik Konjuhaj <besnik.konjuhaj@gmail.com>
2025-01-14 18:35:03 +0200

    Improve tester

--- tester.sh
+++ tester.sh
@@ -5,5 +5,30 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-make re
+DATABASE="db"
 
+
+compile_program() {
+    echo "...Compiling database"
+    make re
+    echo "...Compilation complete"
+}
+
+
+meta_command_test() {
+    output=$(echo ".exit" | ./$DATABASE)
+    test_name="META COMMAND TEST"
+
+ if diff <(echo "$output") "" >/dev/null; then
+        echo "✓ Test passed: $test_name"
+        return 0
+    else
+        echo "✗ Test failed: $test_name"
+        echo "Expected output:"
+        echo "Actual output:"
+        echo "$output"
+        return 1
+    fi
+}
+
+meta_command_test()
